6e872d154ab18bc0315f8834c1ab3d65830fbbbb
bryantuwagbale <78828750+bryantuwagbale@users.noreply.github.com>
2021-08-08 20:13:18 -0500

    deployable to gh pages

---
+++ src/components/About/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function About() {
+    return (
+        <header className="my-5">
+            <h1 id="about">About Me</h1>
+            <div>
+                <p>
+                    My name is Bryant Uwagbale. I am Web Developer who prides himself on his 
+                    ability to continously evolve in any high-intensity enviornment. 
+                </p>
+            </div>
+        </header>
+      );
+}
+
+export default About;
