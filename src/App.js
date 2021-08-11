83f5302951ba10618e2e8e1e1e4b097325ae8b0e
bryantuwagbale <78828750+bryantuwagbale@users.noreply.github.com>
2021-08-11 16:48:07 -0500

    basic structure

--- src/App.js
+++ src/App.js
@@ -1,18 +1,46 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Nav from './components/Nav';
+import Header from './components/Header';
 import About from './components/About';
+import Projects from './components/Projects';
+
+
 
 
 function App() {
   return (
-    <div>
-      <Nav></Nav>
-      <main>
-        <About></About>
-      </main>
-    </div>
+    
+      <Router>
+        <div className="flex-column justify-flex-start min-100-vh">
+          <Header/>
+          <div className="container">
+            {/* <Route exact path="/" component={Home} /> */}
+            <Route exact path="/about" component={About} />
+            <Route exact path="/projects" component={Projects} />
+            {/* <Route exact path="/contact" component={contact} /> */}
+          </div>
+          {/* <Footer /> */}
+        </div>
+      </Router>
+
   );
 }
+
+
+
+
+
+
+// function App() {
+//   return (
+//     <div>
+//       <Nav></Nav>
+//       <main>
+//         <About></About>
+//       </main>
+//     </div>
+//   );
+// }
 
 export default App;
